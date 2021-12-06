a3c8ebc40d3f5356fd2461bbc19b5a24a6a390f8
maciezie <maciej.zielinski@idemia.com>
2021-12-06 21:06:45 +0100

    Advent of code 2021 → Day06 small refactoring of tests

--- src/test/scala/com/mzielinski/aoc2021/day06/Day06Test.scala
+++ src/test/scala/com/mzielinski/aoc2021/day06/Day06Test.scala
@@ -6,25 +6,16 @@ class Day06Test extends org.scalatest.funsuite.AnyFunSuite {
 
   private val fractions = Table(
     ("filename", "result", "days"),
-
     ("day06/00.txt", 26L, 18),
     ("day06/00.txt", 5934L, 80),
     ("day06/01.txt", 371379L, 80),
-
     ("day06/00.txt", 26984457539L, 256),
     ("day06/01.txt", 1674303997472L, 256)
   )
 
   forAll(fractions) { (filename: String, expectedResult: Long, days: Int) =>
     test(s"Day06 - Lanternfish - for file $filename and days: $days") {
-      // given
-      val path = this.getClass.getClassLoader.getResource(filename).getPath
-
-      // when:
-      val result = Day06.run(path, days)
-
-      // then:
-      assert(result == expectedResult)
+      Day06.run(this.getClass.getClassLoader.getResource(filename).getPath, days) == expectedResult
     }
   }
 }
